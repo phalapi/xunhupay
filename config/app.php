58059ce47aa95d046a7f2bddf63c8d3e72fac646
dogstar <chanzonghuang@gmail.com>
2020-01-14 15:05:30 +0800

    up

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    // 虎皮椒支付扩展配置
+    'xunhupay' => array(
+        // 接口地址，一般不需要修改
+        'api' => array(
+            'do' => 'https://api.xunhupay.com/payment/do.html',
+            'query' => 'https://api.xunhupay.com/payment/query.html',
+        ),
+        'appid' => '2147483647', // TODO 修改成你的APPID
+        'appsecret' => '160130736b1ac0d54ed7abe51e44840b', // TODO 修改成你的密钥
+        'notify_url' => '', // TODO 成功支付后的回调地址
+    ),
+);
---
+++ src/Api/Xunhupay.php
@@ -0,0 +1,27 @@
+<?php
+namespace PhalApi\Xunhupay\Api;
+
+/**
+ * 虎皮椒支付
+ */
+class Xunhupay extends \PhalApi\Api {
+
+    public function getRules() {
+        return array(
+        );
+    }
+
+    /**
+     * 发起支付接口
+     * @desc 发起支付接口
+     */
+    public function paymentDo() {
+    }
+
+    /**
+     * 订单查询接口
+     * @desc 订单查询接口
+     */
+    public function orderQuery() {
+    }
+}
